9da414d9f4999838e0ac5e4b91d526b5de2076dd
Darlon Henrique <darlonhenrisouza@gmail.com>
2025-01-22 14:53:22 -0300

    implement status route and test

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,22 @@
 import database from 'infra/database.js'
 
 async function status(request, response) {
-  const result = await database.query('SELECT 1 + 1 as sum;')
-  console.log(result.rows)
-  response.status(200).json({ chave: "meu amigo" })
+  const updatedAt = new Date().toISOString()
+
+  const serverVersionQuery = await database.query('SHOW server_version;')
+  const maxConnectionsQuery = await database.query('SHOW max_connections;')
+  const pgStatActivityQuery = await database.query("SELECT * FROM pg_stat_activity WHERE datname = 'local_db'")
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      postgres: {
+        version: parseInt(serverVersionQuery.rows[0].server_version),
+        max_connections: parseInt(maxConnectionsQuery.rows[0].max_connections),
+        used_connections: pgStatActivityQuery.rows.length
+      }
+    }
+  })
 }
 
 export default status
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,18 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status")
   expect(response.status).toBe(200)
+
+  const responseBody = await response.json()
+  expect(responseBody.updated_at).toBeDefined()
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString()
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt)
+
+  const dependencies = responseBody.dependencies
+
+  expect(dependencies).toBeDefined()
+  expect(dependencies.postgres).toBeDefined()
+  expect(dependencies.postgres.version).toBeDefined()
+  expect(dependencies.postgres.max_connections).toBeDefined()
+  expect(dependencies.postgres.used_connections).toBeDefined()
 })
